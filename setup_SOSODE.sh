bbb9660fe0fa4669fa79898fc9bea9e748993608
xuesoso <xuesoso@gmail.com>
2019-07-19 15:58:00 -0700

    updated setup

--- setup_SOSODE.sh
+++ setup_SOSODE.sh
@@ -2,8 +2,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 pip install jupytext pynvim jupyter-console qtconsole
 cp -r .config/nvim $HOME/.config
 mkdir -p $HOME/.jupyter && cp -r .jupyter/* $HOME/.jupyter/
-mv SOSODE_vimrc $HOME/.SOSODE_vimrc
-vim_call="source '$HOME/.SOSODE_vimrc'"
+cp SOSODE_vimrc $HOME/.SOSODE_vimrc
+vim_call="source $HOME/.SOSODE_vimrc"
 if grep -qF "$vim_call" $HOME/.vimrc;
     then
         echo "vim source line already exists in $HOME/.vimrc";
